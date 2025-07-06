5c82b62e7dbce79bdad51abb97b9c22d3493f293
Deyko Durval da Silva Teixeira <deyko.durval@gmail.com>
2025-07-06 19:42:49 -0300

    feat: remover config.js do gitignore e adicionar ao repo

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://bsajzksjhpositpoflpr.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzYWp6a3NqaHBvc2l0cG9mbHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2OTU3OTMsImV4cCI6MjA2NzI3MTc5M30.RqfY989TR6Qkud7HZo8wE-gzXh6TCCfvyaJ3YFsglhI',
+    SUPABASE_FUNCTION_URL: 'https://bsajzksjhpositpoflpr.supabase.co/functions/v1/gemini-proxy'
+};
+
+window.CONFIG = CONFIG;
